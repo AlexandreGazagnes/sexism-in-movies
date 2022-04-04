67f109ae9cc9140efbe1122b8baccb89ff2a2cf7
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-04-04 18:29:27 +0200

    main  in tests

--- sexism/__init__.py
+++ sexism/__init__.py
@@ -1,5 +1,5 @@
-# from .classifier import Classifier as NudeClassifier
 from ._classifier import Classifier as NudeClassifier
 
+# from .classifier import Classifier as NudeClassifier
 # from .lite_classifier import LiteClassifier as NudeClassifierLite
-from .detector import Detector as NudeDetector
+# from .detector import Detector as NudeDetector
